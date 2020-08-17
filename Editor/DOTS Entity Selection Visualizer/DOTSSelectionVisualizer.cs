7544ac4fb620acda0b690dff279a7676e26c1271
Bennet Jeutter <bennet.jeutter@gmail.com>
2020-08-17 11:01:45 +0200

    Added selection visualizer editor script

---
+++ Editor/DOTS Entity Selection Visualizer/DOTSSelectionVisualizer.cs
@@ -0,0 +1,57 @@
+using Unity.Entities;
+using Unity.Entities.Editor;
+using Unity.Transforms;
+using UnityEditor;
+using UnityEngine;
+
+public class DOTSSelectionVisualizer : EditorWindow
+{
+    [MenuItem("DOTS/Selection Visualizer")]
+    static void Open()
+    {
+        DOTSSelectionVisualizer win = GetWindow<DOTSSelectionVisualizer>();
+        win.titleContent = new GUIContent("DOTS Selection Visualizer");
+        win.Show();
+    }
+    
+    void OnEnable()
+    {
+        SceneView.duringSceneGui += OnSceneGUI;
+    }
+
+    void OnDisable()
+    {
+        SceneView.duringSceneGui -= OnSceneGUI;
+    }
+
+    private void OnSceneGUI(SceneView obj)
+    {
+        var selection = Resources.FindObjectsOfTypeAll<EntitySelectionProxy>();
+        if (selection.Length != 0)
+        {
+            var entity = selection[0].Entity;
+            if (entity == Entity.Null) return;
+
+            var em = selection[0].EntityManager;
+            
+            if (em.HasComponent<Translation>(entity))
+            {
+                var rot = Quaternion.identity;
+                if(em.HasComponent<Rotation>(entity))
+                {
+                    rot = em.GetComponentData<Rotation>(entity).Value;
+                }
+                
+                var translation = em.GetComponentData<Translation>(entity);
+                Handles.PositionHandle(translation.Value, rot);
+                Handles.Label(translation.Value, "Selected Entity");
+                HandleUtility.Repaint();
+            }
+        }
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("Select entity in entity debugger (needs to have rotation and translation component)");
+    }
+}
